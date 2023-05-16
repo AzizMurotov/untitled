28b140e284fcd7218555f002276554c77a25f87b
qahor0v <fluttuzgroup@gmail.com>
2023-05-16 21:23:47 +0500

    second release

--- lib/drawer_page.dart
+++ lib/drawer_page.dart
@@ -1,46 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/home_page.dart';
 
-class DrawerPage extends StatelessWidget {
-  String? name;
-
-  DrawerPage({Key? key, required this.name}) : super(key: key);
+class MySideBar extends StatelessWidget {
+  const MySideBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          Container(
-            padding: EdgeInsets.all(10),
-            color: Colors.grey,
-            height: 200,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const CircleAvatar(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.light_mode),
+          UserAccountsDrawerHeader(
+            accountName: Text('Murotov Aziz'),
+            accountEmail: Text("aziz@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                  "https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png",
+                  fit: BoxFit.cover,
+                  height: 90,
+                  width: 90,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                image: DecorationImage(
+                    image: NetworkImage(
+                      "https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg",
                     ),
-                  ],
+                    fit: BoxFit.cover)),
+          ),
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text('Favorites'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, HomePage.id);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Friends'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.share),
+            title: Text('Share'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Request'),
+            trailing: ClipOval(
+              child: Container(
+                color: Colors.red,
+                height: 20,
+                width: 20,
+                child: Center(
+                  child: Text(
+                    "1",
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(name!),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text("azizmutotov@gmail.com"),
-              ],
+              ),
             ),
           ),
-          Text("dfg"),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.description),
+            title: Text('Policies'),
+            onTap: () => null,
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Exit'),
+            leading: Icon(Icons.exit_to_app),
+            onTap: () => null,
+          ),
         ],
       ),
     );
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,17 +11,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late PageController _pageController;
+  late int _selectIndex;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController = PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer:  DrawerPage(name: 'Muhriddin Yoriqulov',),
+      backgroundColor: Colors.grey,
+      drawer: const MySideBar(),
       appBar: AppBar(
-        title: Text("Side Bar page"),
+        title: Text("Side Bar & Bottom NavBar"),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Drawer"),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (int index) {
+          setState(() {
+            _selectIndex = index;
+          });
+        },
+        children: [
+          Container(
+            color: Colors.pink,
+            child: const Center(
+              child: Text(
+                "PAGE 1",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            child: const Center(
+              child: Text(
+                "PAGE 2",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            child: const Center(
+              child: Text(
+                "PAGE 3",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Account",
+          ),
+        ],
+        currentIndex: _selectIndex,
+        selectedItemColor: Colors.red,
+        onTap: (int itemIndex) {
+          setState(() {
+            _selectIndex = itemIndex;
+            _pageController.animateToPage(itemIndex,
+                duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+          });
+        },
+      ),
+
     );
   }
 }
